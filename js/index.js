4c37defed60b32efb7171f5d9d5d94af77377d28
Tom Ye <tom.ye@chinasystems.com>
2018-06-19 16:41:12 +0800

    remove js

--- js/index.js
+++ js/index.js
@@ -1,34 +1,34 @@
-//var y;
-//var lastY;
-//var max = 0;
-//var min = 0;
-//var fqc = 0;
-//
-//if (window.DeviceMotionEvent) {
-//	window.addEventListener('devicemotion', function (event) {
-//		var acceleration = event.accelerationIncludingGravity;
-//		lastY = y;
-//		y = acceleration.y;
-//		if (y > max) {
-//			max = y;
-//		}
-//		if (y < min) {
-//			min = y;
-//		}
-//		if(lastY * y < 0){
-//			fqc ++;
-//		}
-//	}, false);
-//}
-//
-//alert("开始测试力度，倒计时5秒！");
-//
-//setTimeout(function () {
-//	alert("最大加速度：" + (max - min));
-//
-//	fqc = 0;
-//	alert("开始测试频率，倒计时5秒！");
-//	setTimeout(function () {
-//		alert("五秒内摇动：" + (fqc/2) + "次！");
-//	}, 5000);
-//}, 5000);
+var y;
+var lastY;
+var max = 0;
+var min = 0;
+var fqc = 0;
+
+if (window.DeviceMotionEvent) {
+	window.addEventListener('devicemotion', function (event) {
+		var acceleration = event.accelerationIncludingGravity;
+		lastY = y;
+		y = acceleration.y;
+		if (y > max) {
+			max = y;
+		}
+		if (y < min) {
+			min = y;
+		}
+		if(lastY * y < 0){
+			fqc ++;
+		}
+	}, false);
+}
+
+alert("开始测试力度，倒计时5秒！");
+
+setTimeout(function () {
+	alert("最大加速度：" + (max - min));
+
+	fqc = 0;
+	alert("开始测试频率，倒计时5秒！");
+	setTimeout(function () {
+		alert("五秒内摇动：" + (fqc/2) + "次！");
+	}, 5000);
+}, 5000);
