3cb2c45ac882137fda59878defac24f60bf2437d
Tom Ye <tom.ye@chinasystems.com>
2018-06-19 23:31:31 +0800

    fqc

--- js/index.js
+++ js/index.js
@@ -1,21 +1,8 @@
 $(function () {
-	var y;
-	var lastY;
-	fqc = 0;
 	//GIF封面展示完成后（约9秒）注册click事件方法：switchToPreparePage();
 	setTimeout(function () {
 		$("#main").click(switchToPreparePage);
 	}, 9000);
-
-	if (window.DeviceMotionEvent) {
-		window.addEventListener('devicemotion', function (event) {
-			var acceleration = event.accelerationIncludingGravity;
-			lastY = y;
-			if (lastY * y < 0) {
-				fqc++;
-			}
-		}, false);
-	}
 });
 
 function switchToPreparePage() {
@@ -49,11 +36,22 @@ function goToTestPage() {
 	//加载测试计时页面
 	$("#main").attr("src", "./img/testing.gif");
 
-	alert(fqc);
+	y = 0;
+	lastY = 0;
+	fqc = 0;
+	if (window.DeviceMotionEvent) {
+		window.addEventListener('devicemotion', function (event) {
+			var acceleration = event.accelerationIncludingGravity;
+			lastY = y;
+			if (lastY * y < 0) {
+				fqc++;
+			}
+		}, false);
+	}
 	fqc = 0;
 	setTimeout(function () {
 		var times = fqc / 2;
-		setTimeout(function(){
+		setTimeout(function () {
 			alert("十五秒内摇动了：" + times + "次！");
 		}, 2500);
 	}, 15000);
