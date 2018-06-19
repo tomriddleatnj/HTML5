444b21d1673065e687baafb0915cf965e4d21403
Tom Ye <tom.ye@chinasystems.com>
2018-06-19 22:49:18 +0800

    大致流程

--- js/index.js
+++ js/index.js
@@ -1,34 +1,55 @@
-var y;
-var lastY;
-var max = 0;
-var min = 0;
-var fqc = 0;
+$(function () {
+	alert("页面加载完成");
+	var y;
+	var lastY;
+	var fqc = 0;
+	//GIF封面展示完成后（约9秒）注册click事件方法：switchToPreparePage();
+	setTimeout(function () {
+		$("#main").click(switchToPreparePage);
+	}, 9000);
+	if (window.DeviceMotionEvent) {
+		window.addEventListener('devicemotion', function (event) {
+			var acceleration = event.accelerationIncludingGravity;
+			lastY = y;
+			if (lastY * y < 0) {
+				fqc++;
+			}
+		}, false);
+	}
+});
 
-if (window.DeviceMotionEvent) {
-	window.addEventListener('devicemotion', function (event) {
-		var acceleration = event.accelerationIncludingGravity;
-		lastY = y;
-		y = acceleration.y;
-		if (y > max) {
-			max = y;
-		}
-		if (y < min) {
-			min = y;
-		}
-		if(lastY * y < 0){
-			fqc ++;
-		}
-	}, false);
+function switchToPreparePage() {
+	//解除原click事件
+	$("#main").unbind("click");
+
+	//加载准备页面
+	$("#main").attr("src", "./img/prepare.gif");
+
+	//GIF准备页面展示完成后（约13秒）注册click事件方法：switchToCountDownPage();
+	setTimeout(function () {
+		$("#main").click(switchToCountDownPage);
+	}, 13000);
 }
 
-alert("开始测试力度，倒计时5秒！");
+function switchToCountDownPage() {
+	//解除原click事件
+	$("#main").unbind("click");
 
-setTimeout(function () {
-	alert("最大加速度：" + (max - min));
+	//加载倒计时页面
+	$("#main").attr("src", "./img/countDown.gif");
 
-	fqc = 0;
-	alert("开始测试频率，倒计时5秒！");
-	setTimeout(function () {
-		alert("五秒内摇动：" + (fqc/2) + "次！");
-	}, 5000);
-}, 5000);
+	//GIF准备页面展示完成后（约3秒）跳转至测试页面：goToTestPage();
+	setTimeout(goToTestPage, 3000);
+}
+
+function goToTestPage() {
+	//解除原click事件
+	$("#main").unbind("click");
+
+	//加载测试计时页面
+	$("#main").attr("src", "./img/testing.gif");
+}
+
+//setTimeout(function () {
+//	alert("五秒内摇动：" + (fqc / 2) + "次！");
+//}, 15000);
